e9befa69b5c20a27a940a7af4a94186caf7eb257
TommyLe3825 <tommyle@csu.fullerton.edu>
2019-08-24 15:40:48 -0700

    change hours to amount

--- prob02/main.cpp
+++ prob02/main.cpp
@@ -4,21 +4,21 @@
 
 int main()
 {
-  double hours, rate, pay;
+  double amount, rate, pay;
 
   // Get the number of hours worked.
-  std::cout << "How many hours did you work? ";
-  std::cin >> hours;
+  std::cout << "How many candy bars did you sell? ";
+  std::cin >> amount;
 
   // Get the hourly pay rate.
-  std::cout << "How much do you get paid per hour? ";
+  std::cout << "How much do the organization earn for each bar sold? ";
   std::cin >> rate;
 
   // Calculate the pay.
-  pay = hours * rate;
+  pay = amount * rate;
 
   // Display the pay.
-  std::cout << "You have earned $" << pay << std::endl;
+  std::cout << "Congratulations! You have earned $" << pay << " for the organization!" << std::endl;
 
   return 0;
 }
